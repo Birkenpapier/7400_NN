a7f3c79d8c37b6f032a1aa74e5a46dbe62bca82d
Birkenpapier <alexander.kluender@web.de>
2022-11-30 22:17:27 +0100

    create training data

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 7400_NN
 predicting the output of the 7400 IC using a neural network
+
+## create training data
+```
+g++ NAND_data.cpp -o NAND_data.out && ./NAND_data.out > nand.txt
+```
---
+++ src/NAND_data.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+#define NUMBER_OF_ROWS 5000
+
+int main()
+{
+	for(int i = 0; i <= NUMBER_OF_ROWS; i++)
+	{
+		int a = (int)(2.0 * rand() / double(RAND_MAX));
+		int b = (int)(2.0 * rand() / double(RAND_MAX));
+
+		int t = !(a && b); // should not be 1 or 1
+
+		cout << "in: " << a << ".0 " << b << ".0 " << endl;
+		cout << "out: " << t << ".0" << endl; 
+	}
+
+}
