5dbc9fa5f78285200d55f346edd350fee29e09e6
Birkenpapier <alexander.kluender@web.de>
2022-12-07 00:26:15 +0100

    minor adjustment

--- src/NAND_data.cpp
+++ src/NAND_data.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-	for(int i = 0; i <= NUMBER_OF_ROWS; i++)
+    for(int i = 0; i <= NUMBER_OF_ROWS; i++)
 	{
 		int a = (int)(2.0 * rand() / double(RAND_MAX));
 		int b = (int)(2.0 * rand() / double(RAND_MAX));
@@ -18,5 +18,4 @@ int main()
 		cout << "in: " << a << ".0 " << b << ".0 " << endl;
 		cout << "out: " << t << ".0" << endl; 
 	}
-
 }
